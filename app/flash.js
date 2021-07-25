bc9f915e89f2a57f5b9c2e9ac91aa8c824b7c0d3
Aozasa <oor.as1999@gmail.com>
2021-07-26 08:27:53 +0900

    debian用のパッケージング

--- app/flash.js
+++ app/flash.js
@@ -1,7 +1,9 @@
 const { spawn } = require('child_process');
 const div = document.getElementById('box11');
 const p = document.createElement('p');
-const ret = spawn('cd', ['resources', '&', 'cd','app', '&', 'flash.bat'] ,{ shell: true}); 
+// パスをespの保存フォルダに変更する
+// 環境変数が読み込まれないのでexport.shを実行する必要あり
+const ret = spawn('cd', ['/home/user/esp/hello_world', '&&', '.', '&&',  '/home/user/esp/esp-idf/install.sh ', '&&', '.', '&&',  '/home/user/esp/esp-idf/export.sh ', '&&', 'make','flash'] ,{ shell: true}); 
 ret.stderr.on('data', (err) => {
     console.log(err); 
     p.textContent += err;
